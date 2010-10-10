fc90a1618bd34de990a4a64bd8aa932778d859d8
Max Sobell <max.sobell@cs.nyu.edu>
2010-10-09 20:04:30 -0400

    Code cleanup

--- latlong_twilio.py
+++ latlong_twilio.py
@@ -8,8 +8,8 @@ From lat-lon location, returns a list of venues using the Foursquare API
 import sys
 import os
 import httplib2
-from xml.dom.minidom import parse, parseString
 import foursquare
+import twilio
 
 def usage():
     sys.stdout.write( __doc__ % os.path.basename(sys.argv[0]))
@@ -24,23 +24,9 @@ if __name__ == "__main__":
     lon = sys.argv[2]
     lim = 20
 
-    # url = 'http://api.foursquare.com/v1/venues?geolat=%s&geolong=%s&l=%s' % (lat,lon,lim)
-
-    # print "Fetching from " + url
-
-    # h = httplib2.Http(".cache")
-    # resp, content = h.request(url, "GET")
-
-    # fh = open( 'testresp.xml', 'w' )
-    # fh.write( content )
-    # fh.close()
-
-    # dom = xml.dom.minidom.parseString(content)
-
-    fh = open( 'testresp.xml', 'r' )
     f = foursquare.Api()
     x = f.get_venues(lat,lon,l=lim)
     for i in x['groups'][0]['venues']:
         print i['name']
 
-
+    
